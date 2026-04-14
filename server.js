07707b0d19a31e7927d53fccc283851b97347c4f
Your Name <153941695+Maks-Mm@users.noreply.github.com>
2026-04-14 23:15:39 +0200

    new passwort from google app

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(process.cwd(), "public")));
 app.post("/api/contact", async (req, res) => {
     try {
         const emailUser = (process.env.EMAIL_USER || "").trim();
-        const emailPass = (process.env.EMAIL_PASS || "").replace(/\s/g, "");
+        const emailPass = process.env.EMAIL_PASS;
 
         if (!emailUser || !emailPass) {
             return res.status(500).json({ message: "Missing email credentials" });
@@ -64,3 +64,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+
+//http://localhost:3000/
